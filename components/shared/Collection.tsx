0fe272b6a009cad413e921a1094cdb35f2db3752
dbEsq <d_ber1@live.com>
2024-09-15 19:20:15 -0500

    collection props

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -3,23 +3,24 @@ import Card from './Card'
 import { IEvent } from '@/lib/database/models/event.model'
 
 type CollectionProps = {
-    data: IEvent[],
-    emptyTitle: string,
-    emptyStateSubtext: string,
-    page: number | string,
-    totalPages?: number,
-    collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events',
-    urlParamName?: string,
+  data: IEvent[],
+  emptyTitle: string,
+  emptyStateSubtext: string,
+  limit: number,
+  page: number | string,
+  totalPages?: number,
+  urlParamName?: string,
+  collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
 }
 
 const Collection = ({
-   data,
-   emptyTitle,
-   emptyStateSubtext,
-   page,
-   totalPages=0,
-   collectionType,
-   urlParamName,
+  data,
+  emptyTitle,
+  emptyStateSubtext,
+  page,
+  totalPages = 0,
+  collectionType,
+  urlParamName,
 }: CollectionProps) => {
   return (
     <>
